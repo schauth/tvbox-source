e9fc022502afcfa790d194f6e961118e537c58ed
schauth <75542339+schauth@users.noreply.github.com>
2026-04-10 11:55:57 +0800

    Update replace.js

--- js/replace.js
+++ js/replace.js
@@ -1,5 +1,5 @@
 let body = $response.body;
 body = body.replace(/􄕊􉽃（􄣁􆆰􈪧􉽃）/g, '􄕊􉽃');
-body = body.replace(/2020/g, '2030');
+body = body.replace(/􄼸􉐘􄼸􉐘/g, '2030');
 body = body.replace(/学籍状态：/g, '学籍状态：              在籍');
 $done({body});
